77c998e4cc116b2c6bede7e432e8140803f3a66e
SHargus <87950738+harguss@users.noreply.github.com>
2021-08-30 16:39:01 -0500

    add header

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+class Header extends React.Component {
+  render() {
+    // return whatever you want to render
+    return (
+      <header>
+        <h1>My Fancy Header!</h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
