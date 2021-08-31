e35053c3241fc3c468f7cbc98ef1df71e3a34f12
SHargus <87950738+harguss@users.noreply.github.com>
2021-08-31 17:54:05 -0500

    add lab 2

--- src/Hornedbeast.js
+++ src/Hornedbeast.js
@@ -1,42 +1,23 @@
 import React from 'react';
 
 
-
-
 class Hornedbeast extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { };
-    }
-    // ...
-  } {
-    // return whatever you want to render
-    return (
-//       <div>
-//         <h2>UniWhal</h2>
-//         <img src = "image_url": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",</image>
-//         <p>"A unicorn and a narwhal nuzzling their horns",</p>
-//         // <h2></h2>
-//         // <image></image>
-//         // <h2></h2>
-//         // <image></image>
-//       <div/>
-//     )
+  render(){
+  return(
+  <div>
+    <h2>{this.props.title}</h2>
 
-// }
-// import React from "react";
-//  import ReactDOM from "react-dom";
+    <img title={this.props.title} 
+         src={this.props.src}
+         alt={this.props.title} 
+    />
 
-//  function App() {
-//    return ( 
-//      scr:= "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfasrc="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
-//       alt="new"
-//       />
-//    );
-//  }
-
-//  const rootElement = document.getElementById("root");
- ReactDOM.render(<App />, rootElement);
+    <p>{this.props.description}</p>
+  </div>
+)
+}
+}
+    
 
 
 export default Hornedbeast;
--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,42 @@
 import React from 'react';
+// import data from './data.json';
+import Hornedbeast from './Hornedbeast';
 
 
 class Main extends React.Component {
-  render() {
-    // return whatever you want to render
-    return (
-      <header>
-        <h1></h1>
 
-      </header>
+  render() {
+    return (
+      
       <div>
+        <Hornedbeast 
+        title="UniWhal"
+        src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
+        description="A unicorn and a narwhal nuzzling their horns"
+        />
+         <Hornedbeast 
+        title="Rhino Family"
+        src="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
+        description="Mother (or father) rhino with two babies"
+        />
+        <Hornedbeast 
+        title="Unicorn Head"
+        src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
+        description="Someone wearing a creepy unicorn head mask"
+        />
+        <Hornedbeast
+        title="Basically a unicorn"
+        scr="https://i.pinimg.com/736x/b4/61/06/b46106830b841017ea59870b27ec18dc--narwhals-a-unicorn.jpg"
+        description="A narwhal is basically a unicorn after all, right?"
+        />
         
+        
+
+        {/* {data.map(beast=> (<Hornedbeast title = {beast, title} 
+        image= {beast.image.url} discription {beast,discription} />))} */}
       </div>
     )
-  }
+  };
 }
 
 export default Main;
